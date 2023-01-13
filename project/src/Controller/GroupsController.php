c236d141d359a4547836a100a82eb0896f02291f
Arkansiel92 <yaj92250@hotmail.fr>
2023-01-13 13:45:14 +0100

    back

---
+++ project/src/Controller/GroupsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GroupsController extends AbstractController
+{
+    /**
+     * @Route("/groups", name="app_groups")
+     */
+    public function index(): Response
+    {
+
+        
+
+        return $this->render('groups/index.html.twig', [
+            'controller_name' => 'GroupsController',
+        ]);
+    }
+}
