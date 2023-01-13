a120813e02bfc6a62c0d542d709ea9116debf634
Robin <gdt.robin@gmail.com>
2023-01-13 13:41:50 +0100

    form GroupScreen changed

--- app/src/screens/GroupScreen/GroupScreen.tsx
+++ app/src/screens/GroupScreen/GroupScreen.tsx
@@ -2,11 +2,24 @@ import TextInput from "../../components/TextInput/TextInput";
 
 export default function GroupScreen() {
   return (
-    <div>
-      <TextInput name="name" label="Nom" type="text" />
-      <TextInput name="nbPerson" label="Nombre de personnes" type="text" />
-      <TextInput name="admin" label="Admin" type="text" />
-      <input type="submit" value="Envoyer" />
+    <div className="container my-5 form-group">
+      <h3 className="border-bottom my-3">Groupes</h3>
+      <TextInput name="name" label="Nom" type="text" className="form-control" />
+      <TextInput
+        name="nbPerson"
+        label="Nombre de personnes"
+        type="text"
+        className="form-control"
+      />
+      <div className=" form-group">
+        <TextInput
+          name="admin"
+          label="Admin"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <input className="btn btn-primary" type="submit" value="Valider" />
     </div>
   );
 }
